43d0f1ec6915b1d156a61599a36c3c6098979809
Magnus Hissingby <magnus.h@wemail.no>
2025-09-05 13:27:23 +0200

    core finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   newFriend: string | null = null;
-  favorite: string | null = null;
+  favorite: string | null = "No friends are selected"
 
   people: string[] = ['lewis', 'jules', 'ed', 'nathan', 'dave', 'nigel'];
 
+  setFavorites(fav: string){
+    this.favorite = fav
+  }
   addFriend() {
     if (!this.newFriend) {
       return;
